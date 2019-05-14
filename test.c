423724cbbe5daceedba9bf98fcd1193ad0fa5c9d
qupeng3 <qupeng3.staff.sina.com.cn>
2019-05-14 10:21:10 +0800

    q

---
+++ test.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+ Copyright (C)2019 SangforLtd. All rights reserved.
+	> File Name: test.c
+	> Author: qupeng
+	> Mail: 
+	> Created Time: Tue May 14 10:20:13 2019
+	> Description: 
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(){
+    printf("hello, word.");
+    return 0;
+}
